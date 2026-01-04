4cc52809c4e40e4fe1197427a00fdc4fc9bb31b9
yexiaonanair <yexiaonanair@outlook.com>
2026-01-04 20:06:04 +0800

    Initial commit

---
+++ src/main/java/com/xn/xny/LiteManagerXApplication.java
@@ -0,0 +1,13 @@
+package com.xn.xny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteManagerXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteManagerXApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xn/xny/LiteManagerXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xn.xny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteManagerXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
